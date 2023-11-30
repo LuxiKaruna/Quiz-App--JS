75001d04c7a6fbb7394fa57805eed11d7ba866b5
Luxshi Karunkaran <152510900+LuxiKaruna@users.noreply.github.com>
2023-11-30 20:28:50 +0530

    Quiz App

---
+++ questions.js
@@ -0,0 +1,61 @@
+// Create an array and passing the number, questios, options and answers
+
+let questions=[
+    {
+        number:1,
+        question:"What does HTML stands for?",
+        answer: "Hyper Text Markup Language",
+        options:[
+            "Hyper Text Processor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+        ]
+    },
+    {
+        number:2,
+        question:"What does CSS stands for?",
+        answer: "Cascading Style Sheet",
+        options:[
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        number:3,
+        question:"What does PHP stands for?",
+        answer: "Hypertext Preprocessor",
+        options:[
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "HomeText Preprocessor"
+        ]
+    },
+    {
+        number:4,
+        question:"What does SQL stands for?",
+        answer: "Structured Query Language",
+        options:[
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        number:5,
+        question:"What does XML stands for?",
+        answer: "extensible Markup Laguage",
+        options:[
+            "extensible Markup Laguage",
+            "executable Multiple Language",
+            "eXtra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    }
+
+
+];
